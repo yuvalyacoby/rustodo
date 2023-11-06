a2eaef79fc436d82528d22d03e67bbdf6e7bca42
Yuval Yacoby <yacoby133@gmail.com>
2023-11-06 12:58:09 +0200

    update todo

--- src/server/app.rs
+++ src/server/app.rs
@@ -3,16 +3,16 @@ use axum::{
     http::StatusCode,
     Json, Router, extract::Path,
 };
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize};
 use log;
 
-use crate::todo::{Todo, self};
+use crate::{todo::{Todo, self}, status::Status};
 
 pub fn get_app() -> Router<> {
     // build our application with a route
     let app = Router::new()
         .route("/", get(root))
-        .route("/todo", get(get_todos))
+        .route("/todo", get(get_todos).post(update_todo))
         .route("/todo/:todo_name", get(get_todo));
 
     return app;
@@ -34,31 +34,21 @@ async fn get_todo(Path(todo_name): Path<String>) -> (StatusCode, Json<Todo>) {
     let todo = todo::get_todo(&todo_name).unwrap();
     (StatusCode::ACCEPTED, Json(todo))
 }
-// async fn get_todos(
-//     // this argument tells axum to parse the request body
-//     // as JSON into a `CreateUser` type
-//     Json(payload): Json<CreateUser>,
-// ) -> (StatusCode, Json<User>) {
-//     // insert your application logic here
-//     let user = User {
-//         id: 1337,
-//         username: payload.username,
-//     };
 
-//     // this will be converted into a JSON response
-//     // with a status code of `201 Created`
-//     (StatusCode::CREATED, Json(user))
-// }
+async fn update_todo(
+    Json(payload): Json<UpdateTodoRequest>,
+) -> (StatusCode, Json<Todo>) {
+    log::debug!("update_todo called with {:?}", payload);
+    let todo = todo::update_todo(payload.name, payload.status, payload.description).unwrap();
 
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
+    (StatusCode::CREATED, Json(todo))
 }
 
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
+
+
+#[derive(Deserialize, Debug)]
+struct UpdateTodoRequest {
+    name: String,
+    description: Option<String>,
+    status: Option<Status>
 }
